5f1438997d0c4c73dbe16fc0dbae3ec7ad8a6982
kaznak <kaznak.at.work@gmail.com>
2022-01-23 22:48:37 +0900

    remove example

--- lib/phrase.ts
+++
@@ -1,2 +0,0 @@
-export const phrase = "Hello, Node.js!";
-export default phrase;
--- test/index.test.ts
+++
@@ -1,5 +0,0 @@
-import phrase from "lib/phrase";
-
-test('the phrase includes "hello"', () => {
-  expect(phrase).toMatch(/[Hh][Ee][Ll][Ll][Oo]/);
-});
