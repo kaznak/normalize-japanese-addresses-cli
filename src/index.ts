cfd3898fb5c774c337cd5e249b89199553b3a5d2
kaznak <kaznak.at.work@gmail.com>
2022-02-04 03:02:40 +0900

    refine

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,30 @@
+import { pipeline } from "stream/promises";
 import { Command } from "commander";
 import { parse } from "csv-parse";
 import { stringify } from "csv-stringify";
 import { NormalizeJapaneseAddressesCSV } from "lib/stream/normalizeJapaneseAddresses";
 
 function main(indices: Array<number>) {
-  process.stdin
-    .pipe(parse())
-    .pipe(
-      new NormalizeJapaneseAddressesCSV({
-        indices,
-        normalizerConfig: {
-          japaneseAddressesApi: `file://${__dirname}/../dist/japanese-addresses-master/api/ja`,
-        },
-      })
-    )
-    .pipe(stringify())
-    .pipe(process.stdout);
+  return pipeline(
+    process.stdin,
+    parse(),
+    new NormalizeJapaneseAddressesCSV({
+      indices,
+      normalizerConfig: {
+        japaneseAddressesApi: `file://${__dirname}/../dist/japanese-addresses-master/api/ja`,
+      },
+    }),
+    stringify(),
+    process.stdout
+  );
 }
 
 const program = new Command();
 program
   .argument("[indices...]", "field indices(1 origin) of CSV")
-  .action((argIndices: Array<string>) => {
+  .action(async (argIndices: Array<string>) => {
     const indices =
       0 == argIndices.length ? [0] : argIndices.map((i) => parseInt(i) - 1);
-    return main(indices);
+    await main(indices);
   });
 program.parse();
